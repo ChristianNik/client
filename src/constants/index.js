ce0e369ef55942936e590bb0671460048bbf0ff9
Christian <chnikel@gmail.com>
2021-08-31 01:36:49 +0200

    serve local

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,5 @@
 export const API = {
-	itemsUrl: 'http://localhost:3000/items',
-	itemsAddUrl: 'http://localhost:3000/items',
-	itemsRemoveUrl: 'http://localhost:3000/items',
+	itemsUrl: 'http://192.168.178.20:3000/items',
+	itemsAddUrl: 'http://192.168.178.20:3000/items',
+	itemsRemoveUrl: 'http://192.168.178.20:3000/items',
 };
