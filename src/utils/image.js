c0318e92592c92faf7b37e75a474f0bcfd3f50b3
Christian <chnikel@gmail.com>
2021-09-01 04:42:42 +0200

    change compress options

--- src/utils/image.js
+++ src/utils/image.js
@@ -4,10 +4,10 @@ export async function compressImage(file) {
 	return new Promise((resolve, reject) => {
 		Compress.imageFileResizer(
 			file, // the file from input
-			200, // width
-			200, // height
+			500, // width
+			500, // height
 			'JPEG', // compress format WEBP, JPEG, PNG
-			70, // quality
+			100, // quality
 			0, // rotation
 			(uri) => {
 				resolve(uri);
