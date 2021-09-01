2cc853502b37e5d7d861527eb7faf4b6cea88eb9
Christian <chnikel@gmail.com>
2021-09-01 06:34:58 +0200

    tags input styled

--- src/components/hashtags/hashtags.component.jsx
+++ src/components/hashtags/hashtags.component.jsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import './hashtags.style.css';
 
 const Hashtags = (props) => {
 	return (
 		<div className='Hashtags'>
-			<div>
+			<div
+				style={{
+					display: 'flex',
+					flexDirection: 'column',
+					position: 'relative',
+					...props.labelStyle,
+				}}
+			>
 				<input
 					style={{
 						width: '100%',
@@ -25,22 +32,44 @@ const Hashtags = (props) => {
 						}
 					}}
 				/>
+
+				<div
+					style={{
+						top: 0,
+						bottom: 0,
+						left: '12px',
+						// display: 'flex',
+						alignItems: 'center',
+						width: '100%',
+						outline: 'none',
+						borderRadius: '8px',
+						background: 'hsl(220, 13%, 16%)',
+						color: '#fff',
+						padding: '12px 16px',
+						minHeight: '43px',
+						maxHeight: '62px',
+						overflowY: 'auto',
+						overflowX: 'hidden',
+					}}
+				>
+					{props.tags &&
+						props.tags.map((tag) => (
+							<span
+								key={tag}
+								style={{
+									fontWeight: 'bold',
+									marginRight: '8px',
+								}}
+								onContextMenu={(e) => {
+									e.preventDefault();
+									props.onRemove && props.onRemove(tag);
+								}}
+							>
+								#{tag}
+							</span>
+						))}
+				</div>
 			</div>
-			<p>
-				{props.tags &&
-					props.tags.map((tag) => (
-						<span
-							key={tag}
-							style={{ marginRight: '8px' }}
-							onContextMenu={(e) => {
-								e.preventDefault();
-								props.onRemove && props.onRemove(tag);
-							}}
-						>
-							#{tag}
-						</span>
-					))}
-			</p>
 		</div>
 	);
 };
