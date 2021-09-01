fecc6985af6a6c69c78be28992a11586b196ec5c
Christian <chnikel@gmail.com>
2021-09-02 01:06:02 +0200

    pwa testing

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite';
 import reactRefresh from '@vitejs/plugin-react-refresh';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [reactRefresh()],
+	plugins: [
+		reactRefresh(),
+		VitePWA({
+			manifest: require('./src/pwa/manifest.json'),
+		}),
+	],
 	server: {
 		host: true,
 	},
