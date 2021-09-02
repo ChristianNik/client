6e7f060be875c737239d670be4c02007ee4bf70e
Christian <chnikel@gmail.com>
2021-09-02 02:18:50 +0200

    fixed: cant compress image on PWA

--- src/utils/image.js
+++ src/utils/image.js
@@ -2,19 +2,23 @@ import Compress from 'react-image-file-resizer';
 
 export async function compressImage(file) {
 	return new Promise((resolve, reject) => {
-		Compress.imageFileResizer(
-			file, // the file from input
-			500, // width
-			500, // height
-			'JPEG', // compress format WEBP, JPEG, PNG
-			100, // quality
-			0, // rotation
-			(uri) => {
-				resolve(uri);
-				// You upload logic goes here
-			},
-			'base64' // blob or base64 default base64
-		);
+		try {
+			Compress.default.imageFileResizer(
+				file, // the file from input
+				500, // width
+				500, // height
+				'JPEG', // compress format WEBP, JPEG, PNG
+				100, // quality
+				0, // rotation
+				(uri) => {
+					resolve(uri);
+					// You upload logic goes here
+				},
+				'base64' // blob or base64 default base64
+			);
+		} catch (error) {
+			alert(error);
+		}
 	});
 }
 
