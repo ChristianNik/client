bc263db554d13ad33b0acba1298c61e3b9feda50
Christian <chnikel@gmail.com>
2021-09-03 03:19:15 +0200

    added language support

--- src/components/sidebar/sidebar.component.jsx
+++ src/components/sidebar/sidebar.component.jsx
@@ -8,6 +8,7 @@ import {
 	faCog,
 } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useLanguage } from '../../context/language.context';
 
 const iconStyle = {
 	display: 'flex',
@@ -17,17 +18,25 @@ const iconStyle = {
 	textDecoration: 'none',
 	color: '#fff',
 	opacity: 0.6,
-	// padding: '0 32px',
 };
 
 const navLinkProps = {
+	exact: true,
 	style: iconStyle,
 	activeStyle: {
 		opacity: 1,
 	},
 };
 
+const NavItem = ({ to, icon, text }) => (
+	<NavLink to={to} {...navLinkProps}>
+		<FontAwesomeIcon icon={icon} size='lg' />
+		<small style={{ marginTop: '4px' }}>{text}</small>
+	</NavLink>
+);
+
 const Sidebar = (props) => {
+	const { lang } = useLanguage();
 	return (
 		<nav
 			className='Sidebar'
@@ -49,22 +58,31 @@ const Sidebar = (props) => {
 					minHeight: '56px',
 				}}
 			>
-				<NavLink to='/' exact {...navLinkProps}>
-					<FontAwesomeIcon icon={faHome} size='lg' />
-					<span>Home</span>
-				</NavLink>
-				<NavLink to='/items' {...navLinkProps}>
-					<FontAwesomeIcon icon={faClipboard} size='lg' />
-					<span>Items</span>
-				</NavLink>
-				<NavLink to='/items/add' {...navLinkProps}>
-					<FontAwesomeIcon icon={faPlus} size='lg' />
-					<span>Add</span>
-				</NavLink>
-				<NavLink to='/settings' {...navLinkProps}>
-					<FontAwesomeIcon icon={faCog} size='lg' />
-					<span>Settings</span>
-				</NavLink>
+				<NavItem
+					to='/'
+					{...navLinkProps}
+					icon={faHome}
+					text={lang('ui/sidebar/navigation', 'homeLabel')}
+				/>
+
+				<NavItem
+					to='/items'
+					{...navLinkProps}
+					icon={faClipboard}
+					text={lang('ui/sidebar/navigation', 'itemsLabel')}
+				/>
+				<NavItem
+					to='/items/add'
+					{...navLinkProps}
+					icon={faPlus}
+					text={lang('ui/sidebar/navigation', 'addLabel')}
+				/>
+				<NavItem
+					to='/settings'
+					{...navLinkProps}
+					icon={faCog}
+					text={lang('ui/sidebar/navigation', 'settingsLabel')}
+				/>
 			</div>
 		</nav>
 	);
