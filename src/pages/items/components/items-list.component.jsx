e70ea950b3e76abfcf6ea26bcd46bbf22e03bc63
Christian <chnikel@gmail.com>
2021-09-04 06:58:24 +0200

    item list ui rework

--- src/pages/items/components/items-list.component.jsx
+++ src/pages/items/components/items-list.component.jsx
@@ -3,7 +3,7 @@ import ItemsListItem from './items-list-item.component';
 
 const ItemsList = ({ items, ...rest }) => {
 	return (
-		<ul style={{ display: 'grid', gap: '18px' }}>
+		<ul style={{ display: 'grid', gap: '8px' }}>
 			{(items || []).map((item) => (
 				<ItemsListItem key={item.id} item={item} {...rest} />
 			))}
