5e2060cd911598d5839eedc88cbd645c58444012
Christian <chnikel@gmail.com>
2021-09-05 00:14:50 +0200

    add item: added wrapper

--- src/pages/item-add/item-add.page.jsx
+++ src/pages/item-add/item-add.page.jsx
@@ -32,6 +32,19 @@ function FullWidthButton({ children, onClick }) {
 	);
 }
 
+const Wrapper = (props) => (
+	<div
+		style={{
+			width: '100%',
+			margin: '0 auto',
+			maxWidth: '600px',
+			overflow: 'auto',
+			height: '100%',
+		}}
+		{...props}
+	/>
+);
+
 const ItemAdd = () => {
 	const { lang } = useLanguage();
 	const { items, addItem } = useItems();
@@ -81,171 +94,180 @@ const ItemAdd = () => {
 		<Dialog>
 			<MobileLayout
 				top={
-					<div
-						style={{
-							display: 'flex',
-							alignItems: 'center',
-						}}
-					>
-						<Route exact path='/items/add'>
-							<IconButton
-								icon={faTimes}
-								onClick={() => {
-									history.push('/items');
-								}}
-							/>
-						</Route>
-						<Route exact path={['/items/add/details', '/items/add/valuation']}>
-							<IconButton icon={faChevronLeft} onClick={prevPage} />
-						</Route>
-
-						<h2
+					<Wrapper>
+						<div
 							style={{
-								textAlign: 'center',
-								margin: '24px 0',
+								display: 'flex',
+								alignItems: 'center',
 							}}
 						>
 							<Route exact path='/items/add'>
-								{lang('ui/items/add', 'typeTabTitle')}
+								<IconButton
+									icon={faTimes}
+									onClick={() => {
+										history.push('/items');
+									}}
+								/>
 							</Route>
-							<Route exact path='/items/add/details'>
-								{lang('ui/items/add', 'detailsTabTitle')}
+							<Route
+								exact
+								path={['/items/add/details', '/items/add/valuation']}
+							>
+								<IconButton icon={faChevronLeft} onClick={prevPage} />
 							</Route>
-							<Route exact path='/items/add/valuation'>
-								{lang('ui/items/add', 'valuationTabTitle')}
-							</Route>
-						</h2>
-					</div>
+
+							<h2
+								style={{
+									textAlign: 'center',
+									margin: '24px 0',
+								}}
+							>
+								<Route exact path='/items/add'>
+									{lang('ui/items/add', 'typeTabTitle')}
+								</Route>
+								<Route exact path='/items/add/details'>
+									{lang('ui/items/add', 'detailsTabTitle')}
+								</Route>
+								<Route exact path='/items/add/valuation'>
+									{lang('ui/items/add', 'valuationTabTitle')}
+								</Route>
+							</h2>
+						</div>
+					</Wrapper>
 				}
 				bottom={
+					<Wrapper>
+						<div
+							style={{
+								padding: '16px',
+							}}
+						>
+							<Route exact path='/items/add'>
+								<FullWidthButton
+									onClick={() => history.push(`/items/add/details`)}
+								>
+									{lang('ui/items/add', 'nextLabel')}
+								</FullWidthButton>
+							</Route>
+							<Route exact path='/items/add/details'>
+								<FullWidthButton
+									onClick={() => history.push(`/items/add/valuation`)}
+								>
+									{lang('ui/items/add', 'nextLabel')}
+								</FullWidthButton>
+							</Route>
+							<Route exact path='/items/add/valuation'>
+								<FullWidthButton onClick={handleAddItem}>
+									{lang('ui/items/add', 'add')}
+								</FullWidthButton>
+							</Route>
+						</div>
+					</Wrapper>
+				}
+			>
+				<Wrapper>
 					<div
 						style={{
 							padding: '16px',
+							overflow: 'auto',
 						}}
 					>
-						<Route exact path='/items/add'>
-							<FullWidthButton
-								onClick={() => history.push(`/items/add/details`)}
-							>
-								{lang('ui/items/add', 'nextLabel')}
-							</FullWidthButton>
-						</Route>
-						<Route exact path='/items/add/details'>
-							<FullWidthButton
-								onClick={() => history.push(`/items/add/valuation`)}
-							>
-								{lang('ui/items/add', 'nextLabel')}
-							</FullWidthButton>
-						</Route>
-						<Route exact path='/items/add/valuation'>
-							<FullWidthButton onClick={handleAddItem}>
-								{lang('ui/items/add', 'add')}
-							</FullWidthButton>
-						</Route>
-					</div>
-				}
-			>
-				<div
-					style={{
-						padding: '16px',
-						overflow: 'auto',
-					}}
-				>
-					<form onSubmit={handleAddItem}>
-						<Route exact path='/items/add'>
-							<div
-								style={{
-									overflow: 'hidden',
-								}}
-							>
-								<ul
+						<form onSubmit={handleAddItem}>
+							<Route exact path='/items/add'>
+								<div
 									style={{
-										listStyle: 'none',
-										display: 'grid',
-										gap: '8px',
 										overflow: 'hidden',
 									}}
 								>
-									{itemTypes.map((type) => {
-										return (
-											<li
-												key={type}
-												style={{
-													display: 'block',
-													padding: '8px 16px',
-													border: '2px solid currentColor',
-													borderRadius: '4px',
-													textDecoration: 'none',
-													textAlign: 'center',
-													textTransform: 'uppercase',
-													color: 'hsl(220, 13%, 50%)',
-													...(formData.type === type && {
-														color: '#fff',
-													}),
-												}}
-												onClick={() => {
-													handleTypeChange(type);
-												}}
-												onDoubleClick={() => {
-													handleTypeChange(type);
-													history.push(`/items/add/details`);
-												}}
-											>
-												{type}
-											</li>
-										);
-									})}
-								</ul>
-							</div>
-						</Route>
-
-						<Route exact path='/items/add/details'>
-							<>
-								<div style={{ display: 'flex', justifyContent: 'center' }}>
-									<Avatar
-										src={formData.image}
-										size='xl'
-										onClick={handleSelectImage}
+									<ul
 										style={{
-											marginRight: '16px',
+											listStyle: 'none',
+											display: 'grid',
+											gap: '8px',
+											overflow: 'hidden',
+										}}
+									>
+										{itemTypes.map((type) => {
+											return (
+												<li
+													key={type}
+													style={{
+														display: 'block',
+														padding: '8px 16px',
+														border: '2px solid currentColor',
+														borderRadius: '4px',
+														textDecoration: 'none',
+														textAlign: 'center',
+														textTransform: 'uppercase',
+														color: 'hsl(220, 13%, 50%)',
+														...(formData.type === type && {
+															color: '#fff',
+														}),
+													}}
+													onClick={() => {
+														handleTypeChange(type);
+													}}
+													onDoubleClick={() => {
+														handleTypeChange(type);
+														history.push(`/items/add/details`);
+													}}
+												>
+													{type}
+												</li>
+											);
+										})}
+									</ul>
+								</div>
+							</Route>
+
+							<Route exact path='/items/add/details'>
+								<>
+									<div style={{ display: 'flex', justifyContent: 'center' }}>
+										<Avatar
+											src={formData.image}
+											size='xl'
+											onClick={handleSelectImage}
+											style={{
+												marginRight: '16px',
+											}}
+										/>
+									</div>
+									<hr
+										style={{
+											margin: '16px 0',
+											borderColor: 'hsl(220, 13%, 50%)',
 										}}
 									/>
-								</div>
-								<hr
-									style={{
-										margin: '16px 0',
-										borderColor: 'hsl(220, 13%, 50%)',
-									}}
-								/>
-								<h3>{lang('ui/items/add', 'tagsCaption')}</h3>
-								<Hashtags
-									tags={formData.tags}
-									onSubmit={addTag}
-									onRemove={removeTag}
-								/>
-								<Input
-									name='description'
-									text={lang('ui/items/add', 'descriptionCaption')}
-									value={formData.description}
+									<h3>{lang('ui/items/add', 'tagsCaption')}</h3>
+									<Hashtags
+										tags={formData.tags}
+										onSubmit={addTag}
+										onRemove={removeTag}
+									/>
+									<Input
+										name='description'
+										text={lang('ui/items/add', 'descriptionCaption')}
+										value={formData.description}
+										onChange={handleInputChange}
+									/>
+								</>
+							</Route>
+
+							<Route exact path='/items/add/valuation'>
+								<Rating
+									text={lang('ui/items/add', 'convenienceCaption')}
+									name='valuationConvenience'
 									onChange={handleInputChange}
 								/>
-							</>
-						</Route>
-
-						<Route exact path='/items/add/valuation'>
-							<Rating
-								text={lang('ui/items/add', 'convenienceCaption')}
-								name='valuationConvenience'
-								onChange={handleInputChange}
-							/>
-							<Rating
-								text={lang('ui/items/add', 'appearanceCaption')}
-								name='valuationAppearance'
-								onChange={handleInputChange}
-							/>
-						</Route>
-					</form>
-				</div>
+								<Rating
+									text={lang('ui/items/add', 'appearanceCaption')}
+									name='valuationAppearance'
+									onChange={handleInputChange}
+								/>
+							</Route>
+						</form>
+					</div>
+				</Wrapper>
 			</MobileLayout>
 		</Dialog>
 	);
